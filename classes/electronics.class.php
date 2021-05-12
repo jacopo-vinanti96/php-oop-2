04351b3a25d65c399c686064a58e7a4fe59bae11
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-05-12 17:05:34 +0200

    Prodotto stampato

--- classes/electronics.class.php
+++ classes/electronics.class.php
@@ -4,9 +4,9 @@ class Electronics extends Product
         public $type;
         public $warranty;
     
-        public function __construct($_name, $_brand, $_origin, $_size = NULL, $_color = NULL, $_price, $_images, $_weight = NULL, $_type, $_warranty = FALSE)
+        public function __construct($_name, $_brand, $_origin, $_size = NULL, $_color = NULL, $_price, $_images, $_description, $_weight = NULL, $_type, $_warranty = FALSE)
         {
-            parent::__construct($_name, $_brand, $_origin, $_size = NULL, $_color = NULL, $_price, $_images, $_weight = NULL);
+            parent::__construct($_name, $_brand, $_origin, $_size = NULL, $_color = NULL, $_price, $_images, $_description, $_weight = NULL);
             $this->type = $_type;
             $this->warranty = $_warranty;
         }
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-    include __DIR__."classes/products.class.php";
-    include __DIR__."classes/electronics.class.php";
+    include __DIR__."/classes/products.class.php";
+    include __DIR__."/classes/electronics.class.php";
     
-    $elec1 = new Electronics("Telecomodità", "Delpino", "Italia", "" , "Nero", 5, "",  "", "Telecomando", "");
+    $elec1 = new Electronics("Telecomodità", "Delpino", "Italia", "", "Nero", 5, "https://media.gettyimages.com/photos/low-angle-view-of-remote-control-against-white-background-picture-id1094028672?k=6&m=1094028672&s=612x612&w=0&h=Hklv66o2-XH__-vGFWl6FObUfVtz8PbjxlaIrqoS6iE=", "Un telecomando ergonomico e leggero che si adatta ad ogni tipo di superfice.",  "", "Telecomando", "");
 ?>
 
 
@@ -16,6 +16,21 @@
     <title>E-commerce</title>
 </head>
 <body>
-    <h2></h2>
+    <h2><?php echo $elec1->name ?></h2>
+    <ul>
+        <li>
+        <?php echo "Marca: " . $elec1->brand ?> <br>
+        </li>
+        <li>
+        <?php echo "Paese di origine: " . $elec1->origin ?> <br>
+        </li>
+        <li>
+        <?php echo "Prezzo: " . $elec1->price . "€"  ?> <br>
+        </li>
+    </ul>
+    <img src=<?php echo $elec1->images ?> alt=<?php echo $elec1->name . " illustrazione" ?>>
+    <p>
+        <?php echo $elec1->description ?>
+    </p>
 </body>
 </html>
